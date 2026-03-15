4e1f58231cc9b11c2ccda1ba7aec15566bc251f6
SuchuMichail <michail_suchu@mail.ru>
2026-03-15 20:05:07 +0600

    text change

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('testButton').addEventListener('click', function() {
-    document.getElementById('message').innerText = 'Привет! Это сообщение из фийла со скриптом.';
+    document.getElementById('message').innerText = 'Привет! Это сообщение из фaйла со скриптом.';
 });
